9a69bcfbc46887d9f480a29bbb5af3708c31f8f9
Blynks <eremtek@gmail.com>
2016-04-25 00:05:49 -0400

    Changed file name

--- Qualification-2016-CountingSheep/CountingSheep.java
+++ Qualification-2016-CountingSheep/CountingSheep.java
@@ -1,7 +1,7 @@
 import java.util.*;
 import java.io.*;
 
-public class TheLastWord {
+public class CountingSheep {
 	public static void main(String[] args) {
 		Scanner in = new Scanner(new BufferedReader(new InputStreamReader(System.in)));
 		int t = in.nextInt(); // Scanner has functions to read ints, longs,
